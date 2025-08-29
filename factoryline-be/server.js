af0f79c35807b03fa3af97d6eab9d0720e5c89fb
Arpit Tripathi <tarpit230@gmail.com>
2025-08-29 21:56:22 +0530

    fixed cors error

--- factoryline-be/server.js
+++ factoryline-be/server.js
@@ -15,7 +15,11 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "*" }
+  cors: {
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT"],
+  },
 });
 
 const tvNamespace = io.of("/tv");
@@ -36,7 +40,6 @@ app.use(
   cors({
     origin: process.env.URL,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
