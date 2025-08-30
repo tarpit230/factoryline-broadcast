878645dabb11e87487bc393cf2c3d0f6163378f6
tarpit230 <118742104+tarpit230@users.noreply.github.com>
2025-08-30 10:37:01 +0530

    Update db.js

--- factoryline-be/db.js
+++ factoryline-be/db.js
@@ -2,11 +2,15 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "factory_broadcast",
-  password: process.env.DB_PASS || "postgres",
-  port: process.env.DB_PORT || 5432,
+  // user: process.env.DB_USER || "postgres",
+  // host: process.env.DB_HOST || "localhost",
+  // database: process.env.DB_NAME || "factory_broadcast",
+  // password: process.env.DB_PASS || "postgres",
+  // port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Render requires SSL
+  },
 });
 
 module.exports = pool;
